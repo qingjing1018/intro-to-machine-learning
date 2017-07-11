3bd5c581b65a9f598141a766600ee7a2735faae2
Jing Qing <jing_qing@comcast.com>
2017-07-11 16:31:36 -0600

    Add rbf kernel and compare accuracy and time

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -67,3 +67,27 @@ accuracy = accuracy_score(pred, labels_test)
 
 print "model accuracy is :", accuracy 
 
+#%%%%
+# change kernel to rbf and compare accuracy and time
+clf = SVC(kernel = 'rbf')
+
+t0 = time()
+clf.fit(features_train, labels_train)
+
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+
+pred = clf.predict(features_test)
+
+print "prediction time:", round(time()-t1, 3), "s"
+
+accuracy = accuracy_score(pred, labels_test)
+
+print "model accuracy is :", accuracy 
+
+
+
+
+
+
