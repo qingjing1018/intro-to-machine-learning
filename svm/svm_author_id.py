be4ea430ca719714931a7386328fd267baceeb92
qingjing1018 <qingjing1018@gmail.com>
2017-07-10 23:05:43 -0600

    Reduce training set size to 1% and test accurcy and training time

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -44,9 +44,26 @@ accuracy = accuracy_score(pred, labels_test)
 
 print accuracy
 
-
-
-
 #########################################################
 
+#%%
+# Reduce the training set size and test accuracy
+features_train = features_train[:len(features_train)/100] 
+labels_train = labels_train[:len(labels_train)/100]
+
+t0 = time()
+
+clf.fit(features_train, labels_train)
+
+print "training time:", round(time()-t0, 3), "s"
+
+t1 = time()
+
+pred = clf.predict(features_test)
+
+print "prediction time:", round(time()-t1, 3), "s"
+
+accuracy = accuracy_score(pred, labels_test)
+
+print "model accuracy is :", accuracy 
 
