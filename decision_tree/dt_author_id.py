52e094b8f529a624d5530efcb917ba7a89db0166
Jing Qing <jing_qing@comcast.com>
2017-07-13 10:23:27 -0600

    Load libraries

--- decision_tree/dt_author_id.py
+++ decision_tree/dt_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn import tree
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -20,12 +22,10 @@ from email_preprocess import preprocess
 features_train, features_test, labels_train, labels_test = preprocess()
 
 
+#%%%%%%%
 
 
-#########################################################
-### your code goes here ###
 
 
-#########################################################
 
 
