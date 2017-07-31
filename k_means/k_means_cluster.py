0c472cfce739fb899ddcb36d3941bfcafa06e960
Jing Qing <qingjing1018@gmail.com>
2017-07-30 18:50:11 -0600

    Use two features for k-means clustering

--- k_means/k_means_cluster.py
+++ k_means/k_means_cluster.py
@@ -4,16 +4,13 @@
     Skeleton code for k-means clustering mini-project.
 """
 
-
-
-
 import pickle
 import numpy
 import matplotlib.pyplot as plt
 import sys
 sys.path.append("../tools/")
 from feature_format import featureFormat, targetFeatureSplit
-
+from sklearn.cluster import KMeans
 
 
 
@@ -36,7 +33,7 @@ def Draw(pred, features, poi, mark_poi=False, name="image.png", f1_name="feature
     plt.savefig(name)
     plt.show()
 
-
+#%%%%
 
 ### load in the dict of dicts containing all the data on each person in the dataset
 data_dict = pickle.load( open("../final_project/final_project_dataset.pkl", "r") )
@@ -53,6 +50,7 @@ features_list = [poi, feature_1, feature_2]
 data = featureFormat(data_dict, features_list )
 poi, finance_features = targetFeatureSplit( data )
 
+#%%%
 
 ### in the "clustering with 3 features" part of the mini-project,
 ### you'll want to change this line to 
@@ -62,11 +60,21 @@ for f1, f2 in finance_features:
     plt.scatter( f1, f2 )
 plt.show()
 
+#%%%
+
 ### cluster here; create predictions of the cluster labels
 ### for the data and store them to a list called pred
 
+# Deploy k-means clustering on the financial_features data, 
+# with 2 clusters specified as a parameter. 
+# Store your cluster predictions to a list called pred,
+
+kmeans = KMeans(n_clusters = 2).fit(finance_features)
+
+pred = kmeans.predict(finance_features)
 
 
+#%%%
 
 ### rename the "name" parameter when you change the number of features
 ### so that the figure gets saved to a different file
@@ -74,3 +82,8 @@ try:
     Draw(pred, finance_features, poi, mark_poi=False, name="clusters.pdf", f1_name=feature_1, f2_name=feature_2)
 except NameError:
     print "no predictions object named pred found, no clusters to plot"
+
+#%%%
+
+
+
