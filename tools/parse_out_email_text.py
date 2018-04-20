1c62a0a42e3cb4952ca07f539c05871f20239efd
Jing Qing <qingjing1018@gmail.com>
2018-04-20 00:11:02 -0600

    Parse a string, stem each word, and put it back into a strnig

--- tools/parse_out_email_text.py
+++ tools/parse_out_email_text.py
@@ -28,16 +28,16 @@ def parseOutText(f):
         text_string = content[1].translate(string.maketrans("", ""), string.punctuation)
 
         ### project part 2: comment out the line below
-        words = text_string
+        # words = text_string
 
         ### split the text string into individual words, stem each word,
         ### and append the stemmed word to words (make sure there's a single
         ### space between each stemmed word)
+        word_list = text_string.split()
+        stemmer = SnowballStemmer("english")   
+        stemmed_list = [stemmer.stem(word) for word in word_list]
+        words = ' '.join(stemmed_list) 
         
-
-
-
-
     return words
 
     
